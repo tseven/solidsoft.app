7f0beb9e262649d98609065add4985b01752e295
Steven Kinley <stevenkinley@stevens-m1-pro.localdomain>
2023-07-27 08:41:55 -0700

    blah2

--- src/routes/pouchdb/+page.svelte
+++ src/routes/pouchdb/+page.svelte
@@ -32,7 +32,7 @@
         const local_db = new PouchDB('my_database');
 		if (local_db) local_db_status = 'connected'
 
-		const remote_db = new PouchDB('http://admin:butt@192.168.1.224:5984/shop1')
+		const remote_db = new PouchDB('http://admin:butt@w1.solidsoft:2053/shop1')
 		if (remote_db) remote_db_status = 'connected'
 		console.log('remote_db', remote_db)
 
